3223d029ca345588af7d6eb12f15ed572722b006
/rdndev <rdninc@mail.ru>
2022-04-13 20:45:39 +0300

    add config

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+   class config {
+      public const SITE_NAME = "Moldova Culture";
+
+      public static function getSiteName(): string {
+         return self::SITE_NAME;
+      }
+   }
+?>
