93e1dbc11705c66057444334a8d031e8d9c9f6a2
Arisha <arina.safina.01@mail.ru>
2022-04-20 01:26:43 +0300

    admin_panel

--- template-parts/admin_header.php
+++ template-parts/admin_header.php
@@ -5,30 +5,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/admin_style.css">
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>{Title}</title>
+    <title>{Admin_panel}</title>
 </head>
 
 <body>
     <header>
         <div class="admin-header-container">
             <div class="admin-header">
-                <div data-component="navbar">
-                    <nav class="admin__navbar">
-                        <div class="mr-4">
-                            <div class="dropdown">
-                                <a class="dropdown-toggle" id="admin" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">
-                                    <img src="content/admin.svg" alt="Admin">
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right rounded-0" aria-labelledby="admin">
-                                    <a class="dropdown-item" href="#">Edit my profile</a>
-                                    <a class="dropdown-item" href="#">Log Out</a>
-                                </div>
+                <div class="admin__navbar">
+                    <div class="mr-4">
+                        <div class="dropdown">
+                            <a class="dropdown-toggle" id="admin" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">
+                                <img src="content/admin.svg" alt="Admin">
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right rounded-0" aria-labelledby="messages">
+                                <a class="dropdown-item" href="#">Edit my profile</a>
+                                <a class="dropdown-item" href="#">Log Out</a>
                             </div>
                         </div>
-                    </nav>
+                    </div>
                 </div>
             </div>
     </header>
