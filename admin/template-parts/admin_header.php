8e4d903893284c2f8132add3f4f965639ca34881
Arisha <arina.safina.01@mail.ru>
2022-04-20 05:37:44 +0300

    admin_panel

--- admin/template-parts/admin_header.php
+++ admin/template-parts/admin_header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/admin_style.css">
-    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="../css/normalize.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>{Admin_panel}</title>
 </head>
@@ -15,11 +15,16 @@
     <header>
         <div class="admin-header-container">
             <div class="admin-header">
-                <div class="admin-header__nav">
-                    
-                </div>
+                <div class="admin-header__nav"></div>
+                <nav class="admin__nav">
+                    <ul>
+                        <li class="nav__item">
+                            Админ
+                        </li>
+                    </ul>
+                </nav>
                 <div class="admin-nav__login">
-                    <a href="#"><img src="content/admin.svg" alt="Admin"></a>
+                    <span><a href="#"><img src="../content/adm.svg" alt="Admin"></a></span>
                 </div>
             </div>
         </div>
---
+++ admin/template-parts/edit.php
@@ -0,0 +1,4 @@
+<?php
+require_once "admin_header.php";
+
+require_once "admin_main.php";
